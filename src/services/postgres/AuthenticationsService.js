e122c9e30a031a105df242f872037db220e76d31
YogiPristiawan <yogistanada252@gmail.com>
2022-01-05 22:59:22 +0700

    feat: authentication service and validator

---
+++ src/services/postgres/AuthenticationsService.js
@@ -0,0 +1,49 @@
+const { Pool } = require('pg')
+const InvariantError = require('../../exceptions/InvariantError')
+
+class AuthenticationsService {
+  constructor() {
+    this._pool = new Pool({
+      host: process.env.PGHOST || 'localhost',
+      port: process.env.PGPORT || '5432',
+      database: process.env.PGDATABASE,
+      user: process.env.PGUSER || 'postgres',
+      password: process.env.PGPASSWORD || '',
+    })
+  }
+
+  async addRefreshToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications (token)',
+      values: [token],
+    }
+
+    await this._pool.query(query)
+  }
+
+  async verifyRefreshToken(token) {
+    const query = {
+      text: 'SELECT * FROM authentications WHERE token = $1',
+      values: [token],
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new InvariantError('Refresh token tidak valid')
+    }
+
+    return result.rows[0]
+  }
+
+  async deleteRefreshToken(token) {
+    const query = {
+      text: 'DELETE FROM authentications WHERE token = $1',
+      values: [token],
+    }
+
+    await this._pool.query(query)
+  }
+}
+
+module.exports = AuthenticationsService
---
+++ src/validator/authentications/index.js
@@ -0,0 +1,34 @@
+const AuthenticationError = require('../../exceptions/AuthenticationError')
+const {
+  PostAuthenticationPayloadSchema,
+  PutAuthenticationPayloadSchema,
+  DeleteAuthenticationPayloadSchema,
+} = require('./schema')
+
+const AuthenticationsValidator = {
+  validatePostAuthenticationPayloadSchema: (payload) => {
+    const { error } = PostAuthenticationPayloadSchema.validate(payload)
+
+    if (error) {
+      throw new AuthenticationError(error.message)
+    }
+  },
+
+  validatePutAuthenticationPayloadSchema: (payload) => {
+    const { error } = PutAuthenticationPayloadSchema.validate(payload)
+
+    if (error) {
+      throw new AuthenticationError(error.message)
+    }
+  },
+
+  validaetDeleteAuthenticationPayloadSchema: (payload) => {
+    const { error } = DeleteAuthenticationPayloadSchema.validate(payload)
+
+    if (error) {
+      throw new AuthenticationError(error.message)
+    }
+  },
+}
+
+module.exports = AuthenticationsValidator
